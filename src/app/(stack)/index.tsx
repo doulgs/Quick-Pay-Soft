2d0ffc611ee96176a5bfb6e1cac2904f978dc9b1
Douglas Domiciano <douglas@publisoft.com.br>
2024-03-14 17:25:05 -0300

    Iicialização

---
+++ src/app/(stack)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>COMPONENTE DE TESTE</Text>
+    </View>
+  );
+}
---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Slot } from "expo-router";
+
+export default function Layout() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <Slot />
+    </>
+  );
+}
